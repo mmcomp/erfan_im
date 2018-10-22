11177266ac97367c8c3b50571432497fbadcf833
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2018-10-22 12:06:31 +0330

    chief managing editor dashboard

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -200,6 +200,47 @@ class UserController {
                 dois: dois
             }
             return view.render('admin.admin', { isLogged: true, user: user, journals: journals, request_count: requestCount, statics: statics})
+        }else if(user.group_id==2){
+            let articles = await Artical.query().where('status', 'submitted').orderBy('created_at', 'desc').limit(10).fetch()
+            let newlySubmitedArticles = articles.toJSON()
+            articles = await Artical.query().where('status', 'under_review').orderBy('created_at', 'desc').limit(10).fetch()
+            let underReviewArticles = articles.toJSON()
+            articles = await Artical.query().where('status', 'published').orderBy('created_at', 'desc').limit(10).fetch()
+            let publishedArticles = articles.toJSON()
+            let users = await User.query().whereNotIn('group_id', [1, 2]).fetch()
+            users = users.toJSON()
+            let statics = {
+                registeredUsers: users,
+                managingEdiors: [],
+                reviewers: [],
+                institutesUniversities: [],
+                authors: []
+            }
+            for(let user of users) {
+                if(user.group_id==4) {
+                    statics.managingEdiors.push(user)
+                }else if(user.group_id==3) {
+                    statics.reviewers.push(user)
+                }else if(user.group_id==5) {
+                    statics.authors.push(user)
+                }
+                if(user.university_institute && statics.institutesUniversities.indexOf(user.university_institute)<0) {
+                    statics.institutesUniversities.push(user.university_institute)
+                }
+            }
+            return view.render('admin.chiefeditor', { 
+                isLogged: true, 
+                user: user, 
+                newlySubmitedArticles: newlySubmitedArticles,
+                underReviewArticles: underReviewArticles,
+                publishedArticles: publishedArticles,
+                statics : statics
+            })
+        }else {
+            session.put('msg', 'You Are Not Authorized to use Dashboard')
+            session.put('msg_type', 'danger')
+    
+            return response.route('home')
         }
     }
 }
