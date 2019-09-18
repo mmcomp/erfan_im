d3beeeded715357d0e512d8009350715b5669083
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2019-09-18 10:37:07 +0430

    .

--- app/Controllers/Http/ArticalController.js
+++ app/Controllers/Http/ArticalController.js
@@ -1390,18 +1390,18 @@ class ArticalController {
             allImages = allImages.concat(theXML.images)
             theData.article_disc = theXML.xml
 
+            theXML = ArticalController.html2xml(theArticle.ack)
+            console.log('ack:')
+            console.log(theXML.images)
+            allImages = allImages.concat(theXML.images)
+            theData.article_ack = theXML.xml
+
             /*
             theXML = ArticalController.html2xml(theArticle.material)
             console.log('material:')
             console.log(theXML.images)
             allImages = allImages.concat(theXML.images)
             theData.article_material = theXML.xml
-
-            theXML = ArticalController.html2xml(theArticle.ack)
-            console.log('ack:')
-            console.log(theXML.images)
-            allImages = allImages.concat(theXML.images)
-            theData.article_ack = theXML.xml
 
             theXML = ArticalController.html2xml(theArticle.results)
             console.log('results:')
