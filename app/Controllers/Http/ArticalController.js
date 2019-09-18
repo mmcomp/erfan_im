0b651143e39306dffd8c69b1664670f9ba1f2904
mehrdad <mehrdad@Mehrdad-MacBook-Air.local>
2019-09-18 10:34:51 +0430

    .

--- app/Controllers/Http/ArticalController.js
+++ app/Controllers/Http/ArticalController.js
@@ -1408,12 +1408,12 @@ class ArticalController {
             console.log(theXML.images)
             allImages = allImages.concat(theXML.images)
             theData.article_results = theXML.xml
-            */
             
 
             if(theArticle.refs.length>0) {
                 theData.article_references = ArticalController.ref2xml(theArticle.refs)
             }
+            */
             
             theData.the_doi = theData.global_doi + '/' + theData.journal_doi + '.' + theData.article_doi
             if(theData.gdoi) {
